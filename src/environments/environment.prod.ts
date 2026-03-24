f93237cc572600da724f0c1dc1d219879ee2e434
Tientcheu Israel <israel01tientcheu@gmail.com>
2026-03-24 11:03:48 +0100

    update Firebase configuration in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey:            'YOUR_API_KEY',
-    authDomain:        'YOUR_PROJECT_ID.firebaseapp.com',
-    projectId:         'YOUR_PROJECT_ID',
-    storageBucket:     'YOUR_PROJECT_ID.appspot.com',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId:             'YOUR_APP_ID',
+    apiKey: 'AIzaSyBKIvfW6bzn74oKQZ3hEJa0lyfZ2m5-1bg',
+    authDomain: 'tilportofoliov2.firebaseapp.com',
+    projectId: 'tilportofoliov2',
+    storageBucket: 'tilportofoliov2.firebasestorage.app',
+    messagingSenderId: '4989753859',
+    appId: '1:4989753859:web:c01cfdaa23a3f1a8528c36',
+    measurementId: 'G-PDVJ201FN9',
   },
 };
