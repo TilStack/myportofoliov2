aa3a02763a7ede468551c3b7d53ef772250eac28
Tientcheu Israel <israel01tientcheu@gmail.com>
2026-03-25 22:44:16 +0100

    remove Firebase configuration from production environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyBKIvfW6bzn74oKQZ3hEJa0lyfZ2m5-1bg',
-    authDomain: 'tilportofoliov2.firebaseapp.com',
-    projectId: 'tilportofoliov2',
-    storageBucket: 'tilportofoliov2.firebasestorage.app',
-    messagingSenderId: '4989753859',
-    appId: '1:4989753859:web:c01cfdaa23a3f1a8528c36',
-    measurementId: 'G-PDVJ201FN9',
-  },
 };
